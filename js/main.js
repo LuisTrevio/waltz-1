bb4cf4dda7b9afa44e256a982f4deb9e17a95ad9
Luis Treviño <97487146+LuisTrevio@users.noreply.github.com>
2024-05-17 16:18:45 -0600

    main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const observer = new IntersectionObserver((entries) => {
         entry.target.classList.add('show');
     }
     else {
-        entry.target.classList.remove('show');
+        //entry.target.classList.remove('show');
     }
     });
 });
