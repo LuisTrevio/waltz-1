fcd36006399771586998847d7acd58bd9cf0ea6f
Luis Treviño <97487146+LuisTrevio@users.noreply.github.com>
2024-06-11 23:56:13 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ hiddenElements.forEach((el) => observer.observe(el));
 function ClickMenu() {
     document.querySelectorAll('.br-1').forEach((result) => {result.classList.toggle('bar1-on');})
 
-    document.querySelectorAll('.br-2').forEach((result) => {result.classList.toggle('bar2-on');})
+    /*document.querySelectorAll('.br-2').forEach((result) => {result.classList.toggle('bar2-on');})*/
 
     document.querySelectorAll('.br-3').forEach((result) => {result.classList.toggle('bar3-on');})
 
